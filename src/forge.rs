43dc49f4da5c3fc576c0cec18a8d66890c58f0ac
Ali Ghaffaari <ali.ghaffaari@uni-bielefeld.de>
2024-08-14 18:34:58 +0200

    Fix parsing FORGe rank file
    The records are separated by tab instead of new line. This commit also
    fixes a bug in enumerating ranking values.

--- src/forge.rs
+++ src/forge.rs
@@ -1,10 +1,20 @@
 use log::{error, warn};
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Seek, SeekFrom};
 
 pub type SiteMap = HashMap<u64, usize>;
 pub type RegSiteMap = HashMap<String, SiteMap>;
+
+fn pretty_trunc(s: &str, n: usize) -> String {
+    let prettify = |x: &str| x.replace("\n", "⏎  ");
+    if s.len() <= n {
+        prettify(s).to_string()
+    } else {
+        prettify(&s[..n]).to_string() + "..."
+    }
+}
 
 pub fn parse_id(id: &str) -> Option<(String, u64)> {
     let tokens: Vec<&str> = id.split(':').collect();
@@ -32,35 +42,59 @@ where
 
     let n = (top * nof_records as f64) as usize;
     let mut smap = RegSiteMap::new();
-    let mut i: usize = 1;
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let tokens = parse_id(&line);
+    let mut r: usize = 1;
+    let mut i: usize = 0;
+    for item in reader.split(b'\t') {
+        match item {
+            Ok(item) => {
+                let rec = String::from_utf8_lossy(&item);
+                match parse_id(&rec) {
+                    Some((region, pos)) => {
+                        let entry = smap.entry(region).or_insert(HashMap::new()).entry(pos);
 
-        match tokens {
-            Some((region, pos)) => {
-                if smap.contains_key(&region) {
-                    let rmap = smap.get(&region).unwrap();
-                    if rmap.contains_key(&pos) {
-                        warn!("Duplicated position: {}", &line);
-                        continue;
+                        match entry {
+                            Entry::Occupied(_) => {
+                                warn!(
+                                    "Duplicated FORGe record (rank: {}): '{}'",
+                                    r,
+                                    pretty_trunc(&rec, 30)
+                                );
+                            }
+                            Entry::Vacant(v) => {
+                                v.insert(r);
+                                i += 1;
+                                if i == n {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    None => {
+                        let message = format!(
+                            "Invalid FORGe record (rank: {}): '{}'",
+                            r,
+                            pretty_trunc(&rec, 30)
+                        );
+                        if nof_records < 2 {
+                            error!("{}", message);
+                            std::process::exit(1);
+                        } else {
+                            warn!("{}", message);
+                        }
                     }
                 }
-                smap.entry(region)
-                    .or_insert(HashMap::new())
-                    .entry(pos)
-                    .or_insert(i);
-                i += 1;
+                r += 1;
             }
 
-            None => {
-                warn!("Invalid input format: {}", &line);
+            Err(_) => {
+                error!("Error reading an item from the rank file");
             }
         }
+    }
 
-        if i > n {
-            break;
-        }
+    if i < n {
+        warn!("Not enough distinct records in the rank file");
     }
 
     smap
